f5e5186f89e55c006f69e2f5e767410161f8803c
RotceHR2D2 <torin02@hotmail.com>
2020-12-10 16:22:48 -0600

    Se inicializo la ruta de la pantalla del whatsapp clone

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+ 
+import 'package:Whatsapp_clone/pages/home.dart';
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp App',
+      initialRoute: '/',
+      routes: {
+        '/' : (_) => HomePage(),
+      },
+    );
+  }
+}
